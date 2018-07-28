aac2303edb661b672df9da052b019fa99031fd5d
udany <long.eagle.goodies@gmail.com>
2018-07-28 20:57:38 -0300

    Day one

--- api/config/config.development.js
+++ api/config/config.development.js
@@ -2,6 +2,13 @@ let gCfg = require('./config.global');
 
 let cfg = JSON.parse(JSON.stringify(gCfg));
 
+cfg.mysql = {
+	host: 'localhost',
+	user: 'root',
+	password: 'admin',
+	database: 'hackatinder'
+};
+
 // Changes go here
 
 export default cfg;
--- database.sql
+++ database.sql
@@ -16,6 +16,137 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `campus_user`
+--
+
+DROP TABLE IF EXISTS `campus_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `campus_user` (
+  `user_id` int(11) NOT NULL,
+  `campus` int(2) NOT NULL,
+  PRIMARY KEY (`user_id`,`campus`),
+  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `campus_user`
+--
+
+LOCK TABLES `campus_user` WRITE;
+/*!40000 ALTER TABLE `campus_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `campus_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `evaluation`
+--
+
+DROP TABLE IF EXISTS `evaluation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `evaluation` (
+  `evaluating_user` int(11) NOT NULL,
+  `evaluated_user` int(11) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `evaluation` tinyint(1) NOT NULL,
+  KEY `evaluating_user_fk` (`evaluating_user`),
+  KEY `evaluated_user_fk` (`evaluated_user`),
+  CONSTRAINT `evaluated_user_fk` FOREIGN KEY (`evaluated_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `evaluating_user_fk` FOREIGN KEY (`evaluating_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='column datetime may be a reserved word';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `evaluation`
+--
+
+LOCK TABLES `evaluation` WRITE;
+/*!40000 ALTER TABLE `evaluation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `evaluation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `match`
+--
+
+DROP TABLE IF EXISTS `match`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `match` (
+  `user_1_id` int(11) NOT NULL,
+  `user_2_id` int(11) NOT NULL,
+  `match_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `user_1_match_fk` (`user_1_id`),
+  KEY `user_2_match_fk` (`user_2_id`),
+  CONSTRAINT `user_1_match_fk` FOREIGN KEY (`user_1_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_2_match_fk` FOREIGN KEY (`user_2_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='app level validation: user1''s id is greater than user2''s id';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `match`
+--
+
+LOCK TABLES `match` WRITE;
+/*!40000 ALTER TABLE `match` DISABLE KEYS */;
+/*!40000 ALTER TABLE `match` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `report_type`
+--
+
+DROP TABLE IF EXISTS `report_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `report_type` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `report_type_name_uindex` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `report_type`
+--
+
+LOCK TABLES `report_type` WRITE;
+/*!40000 ALTER TABLE `report_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `report_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tag_user`
+--
+
+DROP TABLE IF EXISTS `tag_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag_user` (
+  `user_id` int(11) NOT NULL,
+  `tag` varchar(30) NOT NULL,
+  PRIMARY KEY (`user_id`,`tag`),
+  CONSTRAINT `tag_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tag_user`
+--
+
+LOCK TABLES `tag_user` WRITE;
+/*!40000 ALTER TABLE `tag_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -24,20 +155,108 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) DEFAULT NULL,
-  `email` varchar(256) DEFAULT NULL,
-  `password` varchar(64) DEFAULT NULL,
+  `name` varchar(256) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `curso` int(2) unsigned NOT NULL,
+  `turma` char(6) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `status_matricula` int(2) unsigned NOT NULL,
+  `nivel_instrucao` int(2) unsigned NOT NULL,
+  `descricao` text,
+  `orientacao_sexual` int(2) unsigned DEFAULT NULL,
+  `bairro` varchar(64) DEFAULT '',
+  `telefone` varchar(13) DEFAULT '',
+  `on_match_message` text NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `user_block`
+--
+
+DROP TABLE IF EXISTS `user_block`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_block` (
+  `blocking_user_id` int(11) NOT NULL,
+  `blocked_user_id` int(11) NOT NULL,
+  `last_update_datetime` datetime NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`blocking_user_id`,`blocked_user_id`),
+  KEY `blocked_user_fk` (`blocked_user_id`),
+  CONSTRAINT `blocked_user_fk` FOREIGN KEY (`blocked_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `blocking_user_fk` FOREIGN KEY (`blocked_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `user_block`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `user_block` WRITE;
+/*!40000 ALTER TABLE `user_block` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_block` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_report`
+--
+
+DROP TABLE IF EXISTS `user_report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_report` (
+  `reporting_user` int(11) NOT NULL,
+  `reported_user` int(11) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `type_id` bigint(20) unsigned NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `reporting_user_fk` (`reporting_user`),
+  KEY `reported_user_fk` (`reported_user`),
+  KEY `type_id_fk` (`type_id`),
+  CONSTRAINT `reported_user_fk` FOREIGN KEY (`reported_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `reporting_user_fk` FOREIGN KEY (`reporting_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `type_id_fk` FOREIGN KEY (`type_id`) REFERENCES `report_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_report`
+--
+
+LOCK TABLES `user_report` WRITE;
+/*!40000 ALTER TABLE `user_report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_social`
+--
+
+DROP TABLE IF EXISTS `user_social`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_social` (
+  `user_id` int(11) NOT NULL,
+  `network_handle` text,
+  `network_name` varchar(64) NOT NULL DEFAULT '',
+  PRIMARY KEY (`user_id`,`network_name`),
+  CONSTRAINT `user_id_user_social_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_social`
+--
+
+LOCK TABLES `user_social` WRITE;
+/*!40000 ALTER TABLE `user_social` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_social` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +268,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-28 15:06:49
+-- Dump completed on 2018-07-28 20:56:41
